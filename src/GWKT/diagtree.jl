73364f77fffdbfb606305ce6939a56e5bea27b35
Kun Chen <chenkun0228@gmail.com>
2021-11-25 22:50:39 -0500

    build tree should work

--- src/GWKT/diagtree.jl
+++ src/GWKT/diagtree.jl
@@ -1,4 +1,5 @@
 module DiagTree
+using PyCall, Printf
 # export PropagatorKT, Weight, addChild
 export Diagrams, Momentum, Propagator, addMomentum!, addPropagator!, Node, addChild
 
@@ -34,7 +35,8 @@ struct Propagator{W}
 end
 
 mutable struct Node{W}
-    operation::Int #0: multiply, 1: add, 2: subtract
+    id::Int
+    operation::Int #1: multiply, 2: add
     factor::Float64 #symmetry factor, Fermi factor, spin factor
     version::Int128
     excited::Bool #if set to excited, then the current weight needs to be replaced with the new weight
@@ -47,12 +49,14 @@ mutable struct Node{W}
     nodes::Vector{Int} #if the Node is a leaf, then child stores the index of propagator, otherwise, it stores the indices of the child nodes
 
     function Node{W}(_parent = 0) where {W}
-        new{W}(0, 1.0, 0, false, W(0), W(0), _parent, [], [])
+        new{W}(1, 0, 1.0, 0, false, W(0), W(0), _parent, [], [])
     end
-    function Node{W}(operation::Int, factor, propagators = [], nodes = [], _parent = 0) where {W}
-        new{W}(operation, factor, 0, false, W(0), W(0), _parent, propagators, nodes)
+    function Node{W}(id, operation::Int, factor, propagators = [], nodes = [], _parent = 0) where {W}
+        new{W}(id, operation, factor, 0, false, W(0), W(0), _parent, propagators, nodes)
     end
 end
+
+isleaf(node) = (length(node.nodes) == 0) #if a node doesn't have child nodes, it must be a leaf (it must be ponting to propagators)
 
 mutable struct Diagrams{W}
     momenta::Vector{Momentum}
@@ -82,10 +86,11 @@ end
 
 function addNode!(diagrams::Diagrams{W}, operation::Int, factor, propagators = [], nodes = []) where {W}
     tree = diagrams.tree
-    node = Node{W}(operation, factor, propagators, nodes)
+    idx = length(tree) + 1
+    node = Node{W}(idx, operation, factor, propagators, nodes)
     push!(tree, node)
-    diagrams.rootIdx = length(tree)
-    return length(tree) #index of the new node
+    diagrams.rootIdx = idx
+    return idx #index of the new node
 end
 
 compareTidx(Tidx1, Tidx2, hasTimeReversal) = hasTimeReversal ? ((Tidx1 == Tidx2) || (Tidx1 == (Tidx2[2], Tidx2[1]))) : Tidx1 == Tidx2
@@ -120,5 +125,84 @@ function addPropagator!(diagrams::Diagrams{W}, type::Int, order, _Kbasis, _Tidx,
     return length(propagators), true #new propagator
 end
 
+"""
+    showTree(ver4, para::Para; verbose=0, depth=999)
+
+    Visualize the diagram tree using ete3 python package
+
+#Arguments
+- `ver4`: the 4-vertex diagram tree to visualize
+- `para`: parameters
+- `verbose=0`: the amount of information to show
+- `depth=999`: deepest level of the diagram tree to show
+"""
+function showTree(diag::Diagrams; verbose = 0, depth = 999)
+    tree = diag.tree
+    K = diag.momenta
+    root = tree[diag.rootIdx]
+
+    # pushfirst!(PyVector(pyimport("sys")."path"), @__DIR__) #comment this line if no need to load local python module
+    ete = PyCall.pyimport("ete3")
+
+    function info(node)
+        s = "N$(node.id):"
+        if node.operation == 1
+            if (node.factor ≈ 1.0) == false
+                s *= @sprintf("%3.1f", node.factor)
+            end
+            s *= "× "
+        elseif node.operation == 2
+            @assert node.factor ≈ 1.0
+            s *= "+ "
+        else
+            error("not implemented!")
+        end
+        return s
+    end
+
+
+    function treeview(node, level, t = nothing)
+        if isnothing(t)
+            t = ete.Tree(name = " ")
+        end
+
+        nt = t.add_child(name = info(node))
+        name_face = ete.TextFace(nt.name, fgcolor = "black", fsize = 10)
+        nt.add_face(name_face, column = 0, position = "branch-top")
+
+        for child in node.nodes
+            treeview(tree[child], level + 1, nt)
+        end
+        for pidx in node.propagators
+            p = diag.propagators[pidx]
+            nnt = nt.add_child(name = "P$pidx: typ $(p.type), K $(K[p.Kidx].basis), T $(p.Tidx)")
+
+            # name_face = ete.TextFace(nnt.name, fgcolor = "black", fsize = 10)
+            # nnt.add_face(name_face, column = 0, position = "branch-top")
+        end
+
+        return t
+    end
+
+    t = treeview(root, 1)
+    # style = ete.NodeStyle()
+    # style["bgcolor"] = "Khaki"
+    # t.set_style(style)
+
+
+    ts = ete.TreeStyle()
+    ts.show_leaf_name = true
+    # ts.show_leaf_name = True
+    # ts.layout_fn = my_layout
+    ####### show tree vertically ############
+    # ts.rotation = 90 #show tree vertically
+
+    ####### show tree in an arc  #############
+    # ts.mode = "c"
+    # ts.arc_start = -180
+    # ts.arc_span = 180
+    # t.write(outfile="/home/kun/test.txt", format=8)
+    t.show(tree_style = ts)
+end
 
 end
