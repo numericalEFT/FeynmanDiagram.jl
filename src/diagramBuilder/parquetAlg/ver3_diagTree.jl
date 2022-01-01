32875d80fc7f1d049ef2eb45fdc16b48a1af6fa0
Kun Chen <chenkun0228@gmail.com>
2021-12-31 23:54:12 -0500

    working on ver3

--- src/diagramBuilder/parquetAlg/ver3_diagTree.jl
+++ src/diagramBuilder/parquetAlg/ver3_diagTree.jl
@@ -1,30 +1,39 @@
-function buildVer3(para, legK, subdiagram = false; F = [I, U, S], V = [I, T, U], All = union(F, V), diag = newDiagTree(para, :Ver3))
+function buildVer3(para, legK, subdiagram = false; F = [I, U, S], V = [I, T, U], All = union(F, V), chan = All, diag = newDiagTree(para, :Ver3))
     KinR, KoutR = legK[1], legK[2]
-    q = k1 - k2
-    K = zero(k1)
-    K[para.firstLoopIdx] = 1.0
+    q = KinR - KoutR
+    K = zero(KinR)
+    loopIdx = para.firstLoopIdx
     tleft = para.firstTauIdx
+    K[loopIdx] = 1.0
     tauNum = para.interactionTauNum
     maxLoopIdx = para.firstLoopIdx - 1 + para.innerLoopNum
     maxTauIdx = para.firstTauIdx - 1 + (1 + para.innerLoopNum * para.interactionTauNum)
 
     partition = orderedPartition(para.innerLoopNum - 1, 3, 0)
-    for (g1L, verL, g2L) in partition
-        # # example 1: (1, 2, 1), 
-        # # example 2: (0, 2, 0)
-        # G1firstLoopIdx = para.firstLoopIdx + 1 #first inner loop index of G1 (may not exist if G1 loop number is zero) 
-        # verfirstLoopIdx = para.firstLoopIdx + g1L
-        # G2firstLoopIdx = verfirstLoopIdx + verL #first inner loop index of G2 (may not exist if G2 loop number is zero)
-        # loopRight = verfirstLoopIdx + verL + g2L - 1
-        # # eg1: 4+2+1-1 == 6, eg2: 3+2+0-1 == 4
-        # @assert loopRight == maxLoopIdx
+    for p in partition
+        g1L, verL, g2L = p
 
-        # G1firstTauIdx = tleft + 1
-        # verfirstTauIdx = tleft + g1L * tauNum
-        # G2firstTauIdx = verfirstTauIdx + (verL + 1) * TauNum
-        # tauRight = G2firstTauIdx + oGx * TauNum - 1
-        # # eg1: 4+3+1-1==7, eg2: 3+3+0-1==5
-        # @assert tauRight == maxTauIdx(ver4)
+        idx, maxidx = findFirstLoopIdx(p, [true, false, true], loopIdx + 1)
+        g1firstLoopIdx, verfirstLoopIdx, g2firstLoopIdx = idx
+        @assert maxidx == maxLoopIdx
 
+        idx, maxidx = findFirstTauIdx(p, [true, false, true], tleft + 1, tauNum)
+        g1firstTauIdx, verfirstTauIdx, g2firstTauIdx = idx
+        @assert maxidx == maxTauIdx
+
+        g1Para = reconstruct(para, innerLoopNum = g1L, firstLoopIdx = g1firstLoopIdx, firstTauIdx = g1firstTauIdx)
+        g2Para = reconstruct(para, innerLoopNum = g2L, firstLoopIdx = g2firstLoopIdx, firstTauIdx = g2firstTauIdx)
+        verPara = reconstruct(para, innerLoopNum = verL, findFirstLoopIdx = verfirstLoopIdx, firstTauIdx = verfirstTauIdx)
+
+        if isValidG(g1Para) == false || isValidG(g2Para) == false
+            continue
+        end
+
+        Kx = K + KinR - KoutR
+
+        g1, isnode1 = buildG(g1Para, K, [tleft, tleft + 1]; diag = diag)
+        ver4 = buildVer4(verPara, [K, Kx, KinR, KoutR], chan, F, V, All; diag = diag)
+
+        # g1, isnode1 = buildG(g1Para, K, [tleft, tleft]; diag = diag)
     end
 end
