d3203a1ecccd06d5866f2c02f05228640a7af0e8
Kun Chen <chenkun0228@gmail.com>
2022-01-07 15:53:32 -0500

    refactor folder structure

--- src/FeynmanDiagram.jl
+++ src/FeynmanDiagram.jl
@@ -1,11 +1,45 @@
 module FeynmanDiagram
-using Random, LinearAlgebra
+using Random, LinearAlgebra, Parameters
 
-# include("variable.jl")
-# export Var
+@enum DiagramType begin
+    SigmaDiag          #self-energy
+    GreenDiag          #green's function
+    PolarDiag          #polarization
+    Ver3Diag           #3-point vertex function
+    Ver4Diag           #4-point vertex function
+end
 
-# include("GWKT/GWKT.jl")
-# export GWKT
+@enum Filter begin
+    Wirreducible  #remove all polarization subdiagrams
+    Girreducible  #remove all self-energy inseration
+    NoHatree
+    NoFock
+    NoBubble  # true to remove all bubble subdiagram
+    Proper  #ver4, ver3, and polarization diagrams may require to be irreducible along the transfer momentum/frequency
+end
+
+@enum InteractionName begin
+    Composite
+    ChargeCharge
+    SpinSpin
+    ProperChargeCharge
+    ProperSpinSpin
+end
+
+@enum InteractionType begin
+    Instant
+    Dynamic
+    D_Instant #derivative of instant interaction
+    D_Dynamic #derivative of the dynamic interaction
+end
+
+export SigmaDiag, PolarDiag, Ver3Diag, Ver4Diag
+export Wirreducible, Girreducible, NoBubble, NoHatree, Proper
+export InteractionName, ChargeCharge, SpinSpin
+export InteractionType, Instant, Dynamic, D_Instant, D_Dynamic
+
+include("parameter.jl")
+export GenericPara, Interaction
 
 include("diagramTree/DiagTree.jl")
 export DiagTree
