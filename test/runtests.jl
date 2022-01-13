34aa56e721ea6c59f2e127bb867b6e7026c027a1
Kun Chen <chenkun0228@gmail.com>
2022-01-13 17:24:38 -0500

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test, LinearAlgebra, Random, StaticArrays, Printf, Parameters
 include("common.jl")
 include("diagtree.jl")
 include("diagram_count.jl")
-# include("parquet.jl")
+include("parquet.jl")
 # include("newparquet.jl")
 
 # @testset "FeynmanDiagram.jl" begin
