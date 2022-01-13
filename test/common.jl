c09bd0d1eb2d17ca55263d14f05e3e3f063e80fb
Kun Chen <chenkun0228@gmail.com>
2022-01-13 16:09:49 -0500

    wip

--- test/common.jl
+++ test/common.jl
@@ -9,6 +9,14 @@
     aa = reconstruct(a, transferLoop = [0.0, 0.0, 0.0])
     @test a != aa
 
+    # reconstruct with the same diagType but different interaction
     aaa = reconstruct(a, interaction = [])
     @test a != aaa
+
+    # reconstruct with different diagram type leads to different parameter
+    #reconstructed GenericPara uses the old parameters such as firstLoopIdx, totalLoopNum etc., which are different between diagTypes
+    s = reconstruct(a, diagType = SigmaDiag)
+    ss = GenericPara(diagType = SigmaDiag, innerLoopNum = 2)
+    @test s != ss
+
 end
