991c97e2134127311d125b9e3d1aca771cb5e369
Kun Chen <chenkun0228@gmail.com>
2022-01-13 15:50:29 -0500

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using FeynmanDiagram
 using Lehmann
-using Test, LinearAlgebra, Random, StaticArrays, Printf
+using Test, LinearAlgebra, Random, StaticArrays, Printf, Parameters
 
+include("common.jl")
 include("diagtree.jl")
 include("diagram_count.jl")
-include("parquet.jl")
+# include("parquet.jl")
 # include("newparquet.jl")
 
-@testset "FeynmanDiagram.jl" begin
-    # Write your tests here.
-end
+# @testset "FeynmanDiagram.jl" begin
+# Write your tests here.
+# end
