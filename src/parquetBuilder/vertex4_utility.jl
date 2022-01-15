a14cf191a0307bb57b073fac0ca1a7f4e2f66150
Kun Chen <chenkun0228@gmail.com>
2022-01-15 17:33:08 -0500

    clean up

--- src/parquetBuilder/vertex4_utility.jl
+++ src/parquetBuilder/vertex4_utility.jl
@@ -21,6 +21,33 @@ function legBasis(chan::Channel, legK, loopIdx)
     return LLegK, K, RLegK, Kx
 end
 
+function tauBasis(chan::Channel, LvT, RvT)
+    if chan == T
+        extT = (LvT[INL], LvT[OUTL], RvT[INR], RvT[OUTR])
+        G0T = (LvT[OUTR], RvT[INL])
+        GxT = (RvT[OUTL], LvT[INR])
+    elseif chan == U
+        extT = (LvT[INL], RvT[OUTR], RvT[INR], LvT[OUTL])
+        G0T = (LvT[OUTR], RvT[INL])
+        GxT = (RvT[OUTL], LvT[INR])
+    elseif chan == S
+        extT = (LvT[INL], RvT[OUTL], LvT[INR], RvT[OUTR])
+        G0T = (LvT[OUTR], RvT[INL])
+        GxT = (LvT[OUTL], RvT[INR])
+    else
+        error("not implemented!")
+    end
+    return extT, G0T, GxT
+end
+
+function factor(para, chan::Channel)
+    Factor = SymFactor[Int(chan)] / (2π)^para.loopDim
+    if para.isFermi == false
+        Factor = abs(Factor)
+    end
+    return Factor
+end
+
 function typeMap(ltype, rtype)
     if (ltype == Instant || ltype == Dynamic) && (rtype == Instant || rtype == Dynamic)
         return Dynamic
