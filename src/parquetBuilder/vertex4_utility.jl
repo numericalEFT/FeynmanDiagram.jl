87003ba5f0c28731abed8ca3c35c314ff0b245c3
Kun Chen <chenkun0228@gmail.com>
2022-01-15 17:59:23 -0500

    clean up

--- src/parquetBuilder/vertex4_utility.jl
+++ src/parquetBuilder/vertex4_utility.jl
@@ -18,27 +18,39 @@ function legBasis(chan::Channel, legK, loopIdx)
     else
         error("not implemented!")
     end
+
+    # check conservation and momentum assignment
+    @assert LLegK[INL] ≈ KinL
+    @assert LLegK[INL] + LLegK[INR] ≈ LLegK[OUTL] + LLegK[OUTR]
+    @assert RLegK[INL] + RLegK[INR] ≈ RLegK[OUTL] + RLegK[OUTR]
+
     return LLegK, K, RLegK, Kx
 end
 
 function tauBasis(chan::Channel, LvT, RvT)
+    G0T = (LvT[OUTR], RvT[INL])
     if chan == T
         extT = (LvT[INL], LvT[OUTL], RvT[INR], RvT[OUTR])
-        G0T = (LvT[OUTR], RvT[INL])
         GxT = (RvT[OUTL], LvT[INR])
     elseif chan == U
         extT = (LvT[INL], RvT[OUTR], RvT[INR], LvT[OUTL])
-        G0T = (LvT[OUTR], RvT[INL])
         GxT = (RvT[OUTL], LvT[INR])
     elseif chan == S
         extT = (LvT[INL], RvT[OUTL], LvT[INR], RvT[OUTR])
-        G0T = (LvT[OUTR], RvT[INL])
         GxT = (LvT[OUTL], RvT[INR])
     else
         error("not implemented!")
     end
+
+    # make sure all tidx are used once and only once
+    t1 = sort(vcat(collect(G0T), collect(GxT), collect(extT)))
+    t2 = sort(vcat(collect(LvT), collect(RvT)))
+    @assert t1 == t2 "chan $(chan): G0=$G0T, Gx=$GxT, external=$extT don't match with Lver4 $LvT and Rver4 $RvT"
+    @assert extT[INL] == LvT[INL]
     return extT, G0T, GxT
 end
+
+
 
 function factor(para, chan::Channel)
     Factor = SymFactor[Int(chan)] / (2π)^para.loopDim
