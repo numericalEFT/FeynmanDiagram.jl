16bc44343f36b518a7d7eb8527153c07a79368a3
Kun Chen <chenkun0228@gmail.com>
2022-01-17 22:54:42 -0500

    wip

--- src/diagram_tree/diagram.jl
+++ src/diagram_tree/diagram.jl
@@ -5,15 +5,15 @@ Base.show(io::IO, d::DiagramId) = error("Base.show not implemented!")
 Base.isequal(a::DiagramId, b::DiagramId) = error("Base.isequal not implemented!")
 Base.(==)(a::DiagramId, b::DiagramId) = Base.iequal(a, b)
 
-mutable struct Diagram{T<:Diagram}
+struct Diagram{T<:Diagram}
     id::T
-    # parent::Diagram
     subdiagrams::Vector{Diagram}
+    # parent::Diagram
 
     Diagram(id::T) where {T<:DiagramId} = new{T}(id, [])
 end
 
-function addsubdiagram!(parent::Diagram, child::Diagram)
+function add_subdiagram!(parent::Diagram, child::Diagram)
     for c in parent.children
         if c.id == child.id
             return false
@@ -23,5 +23,12 @@ function addsubdiagram!(parent::Diagram, child::Diagram)
 end
 
 function toDataFrame(diagVec::Vector{Diagram})
-
+    df = DataFrame()
+    for diag in diagVec
+        d = toDict(diag.id)
+        d[:hash] = hash(diag.id)
+        d[:subdiagram] = [hash(d) for d in diag.subdiagrams]
+        push!(df, d)
+    end
+    return df
 end
