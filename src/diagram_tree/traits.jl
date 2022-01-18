68c1f05cf566c55b81bb2fd68c4a1609344f60b8
Kun Chen <chenkun0228@gmail.com>
2022-01-18 11:23:22 -0500

    wip

--- src/diagram_tree/traits.jl
+++ src/diagram_tree/traits.jl
@@ -13,9 +13,6 @@ Base.show(io::IO, o::Operator) = print(io, typeof(o))
 Base.isequal(a::Operator, b::Operator) = (typeof(a) == typeof(b))
 Base.:(==)(a::Operator, b::Operator) = Base.isequal(a, b)
 
-apply(o::Operator, weights) = error("not implemented!")
-apply(o::Add, weights::Vector{W}) where {W<:Number} = sum(weights)
-apply(o::Add, weight::Number) = weight
-apply(o::Mutiply, weights::Number) = weight
+apply(o::Operator, diags) = error("not implemented!")
 
 
