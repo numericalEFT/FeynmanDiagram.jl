8b28595d03e3eb64365706da7d31dd379750e24e
Kun Chen <chenkun0228@gmail.com>
2022-01-18 11:02:33 -0500

    wip

--- src/diagram_tree/traits.jl
+++ src/diagram_tree/traits.jl
@@ -12,3 +12,10 @@ struct Mutiply <: Operator end
 Base.show(io::IO, o::Operator) = print(io, typeof(o))
 Base.isequal(a::Operator, b::Operator) = (typeof(a) == typeof(b))
 Base.:(==)(a::Operator, b::Operator) = Base.isequal(a, b)
+
+apply(o::Operator, weights) = error("not implemented!")
+apply(o::Add, weights::Vector{W}) where {W<:Number} = sum(weights)
+apply(o::Add, weight::Number) = weight
+apply(o::Mutiply, weights::Number) = weight
+
+
