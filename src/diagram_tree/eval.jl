d243fa849b780c8d3712b8f6e13c6569c5aa9826
Kun Chen <chenkun0228@gmail.com>
2022-01-20 12:17:06 -0500

    improve eval.jl

--- src/diagram_tree/eval.jl
+++ src/diagram_tree/eval.jl
@@ -3,25 +3,48 @@
 @inline apply(o::Sum, diag::Diagram{W}) where {W<:Number} = diag.weight
 @inline apply(o::Prod, diag::Diagram{W}) where {W<:Number} = diag.weight
 
-function evalDiagNode!(diag::Diagram, evalBare::Function, vargs...; kwargs...)
+function evalDiagNode!(diag::Diagram, evalBare::Function, varK, varT)
     if length(diag.subdiagram) == 0
-        diag.weight = evalBare(diag.id, vargs...; kwargs...) * diag.factor
+        diag.weight = evalBare(diag.id, varK, varT) * diag.factor
     else
         diag.weight = apply(diag.operator, diag.subdiagram) * diag.factor
     end
     return diag.weight
 end
 
-function evalDiagTree!(diag::Diagram, evalBare::Function, vargs...; kwargs...)
+function evalDiagTree!(diag::Diagram, evalBare::Function, varK, varT)
     for d in PostOrderDFS(diag)
-        evalDiagNode!(d, evalBare, vargs...; kwargs...)
+        evalDiagNode!(d, evalBare, varK, varT)
     end
     return diag.weight
 end
 
-function evalDiagTree!(diags::Vector{Diagram{W}}, evalBare::Function, vargs...; kwargs...) where {W}
+function evalDiagTree!(diags::Vector{Diagram{W}}, evalBare::Function, varK, varT) where {W}
     for d in diags
-        evalDiagTree!(d, evalBare, vargs...; kwargs...)
+        evalDiagTree!(d, evalBare, varK, varT)
+    end
+    return W[d.weight for d in diags]
+end
+
+function evalDiagNode!(diag::Diagram, evalBare::Function, vargs...)
+    if length(diag.subdiagram) == 0
+        diag.weight = evalBare(diag.id, vargs...) * diag.factor
+    else
+        diag.weight = apply(diag.operator, diag.subdiagram) * diag.factor
+    end
+    return diag.weight
+end
+
+function evalDiagTree!(diag::Diagram, evalBare::Function, vargs...)
+    for d in PostOrderDFS(diag)
+        evalDiagNode!(d, evalBare, vargs...)
+    end
+    return diag.weight
+end
+
+function evalDiagTree!(diags::Vector{Diagram{W}}, evalBare::Function, vargs...) where {W}
+    for d in diags
+        evalDiagTree!(d, evalBare, vargs...)
     end
     return W[d.weight for d in diags]
 end
