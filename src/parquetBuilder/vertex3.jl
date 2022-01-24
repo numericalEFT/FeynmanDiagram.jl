43d079ebf9434decf8e3d99ae59c878f8e85b250
Kun Chen <chenkun0228@gmail.com>
2022-01-23 22:36:23 -0500

    vertex3 is ready for test

---
+++ src/parquetBuilder/vertex3.jl
@@ -0,0 +1,74 @@
+function Vertex3(para, extK, subdiagram = false; name = :Γ3, chan = [PHr, PHEr, PPr, Alli])
+    (subdiagram == false) && uidreset()
+    @assert para.diagType == Vertex3Diag
+    @assert para.innerLoopNum >= 1 "Only generates vertex corrections with more than one internal loops."
+    @assert length(extK) == para.totalLoopNum
+
+    q, Kin = extK[1], extK[2]
+    Kout = q .+ Kin
+    @assert ((q ≈ Kin) == false) && ((q ≈ Kout) == false) "The bosonic q cann't be same as the fermionic k. Ohterwise the proper diagram check will fail!"
+
+    if Proper in para.filter
+        @assert para.transferLoop ≈ q "To get proper diagrams, please set para.transferLoop as the bosonic momentum."
+    end
+
+    if (para.extra isa ParquetBlocks) == false
+        parquetblocks = ParquetBlocks(phi = [PPr, PHEr], ppi = [PHr, PHEr], Γ4 = [PPr, PHr, PHEr])
+        para = reconstruct(para, extra = parquetblocks)
+    end
+
+    K = zero(q)
+    LoopIdx = para.firstLoopIdx
+    K[LoopIdx] = 1.0
+    t0 = para.firstTauIdx
+    # extT = (t0, t0 + 1)
+    legK = [Kin, Kout, K, K .+ q]
+
+    vertex3 = DataFrame()
+
+    ######################## Π0 = GG #########################################
+    for (oGin, oGout, oVer4) in orderedPartition(para.innerLoopNum - 1, 3, 0)
+
+        idx, maxLoop = findFirstLoopIdx([oGin, oGout, oVer4], LoopIdx + 1)
+        @assert maxLoop <= para.totalLoopNum "maxLoop = $maxLoop > $(para.totalLoopNum)"
+        GinKidx, GoutKidx, Ver4Kidx = idx
+
+        idx, maxTau = findFirstTauIdx([oGin, oGout, oVer4], [GreenDiag, GreenDiag, Ver4Diag], para.firstTauIdx, para.interactionTauNum)
+        @assert maxTau <= para.totalTauNum
+        GinTidx, GoutTidx, Ver4Tidx = idx
+
+        paraGin = reconstruct(para, diagType = GreenDiag, innerLoopNum = oGin,
+            firstLoopIdx = GinKidx, firstTauIdx = GinTidx)
+        paraGout = reconstruct(para, diagType = GreenDiag, innerLoopNum = oGout,
+            firstLoopIdx = GoutKidx, firstTauIdx = GoutTidx)
+        paraVer4 = reconstruct(para, diagType = Ver4Diag, innerLoopNum = oVer4,
+            firstLoopIdx = Ver4Kidx, firstTauIdx = Ver4Tidx)
+
+        if isValidG(paraGin) && isValidG(paraGout)
+            ver4 = buildVer4(paraVer4, legK, chan, true)
+
+            #transform extT coloum into extT for Vertex4 and the extT for Gin and Gout
+            df = transform(ver4, :extT => ByRow(x -> [(t0, x[INL], x[OUTL]), (t0, x[INR]), (x[OUTR], t0)]) => [:extT, :GinT, :GoutT])
+
+            groups = mergeby(df, [:response, :GinT, :GoutT, :extT], operator = Sum())
+
+            for v4 in eachrow(groups)
+                response, type = v4[:response], v4[:type]
+                @assert response == UpUp || response == UpDown
+                #type: Instant or Dynamic
+                ver3id = Ver3Id(para, response, k = extK, t = v4[:extT])
+                gin = buildG(paraGin, K, v4[:GinT], true, name = :Gin)
+                gout = buildG(paraGout, K .+ q, v4[:GoutT], true, name = :Gout)
+                @assert gin isa Diagram && gout isa Diagram
+
+                ver3diag = Diagram(ver3id, Prod(), [gin, gout, v4[:diagram]], name = name)
+                push!(vertex3, (response = response, extT = group[:extT], diagram = ver3diag))
+            end
+        end
+    end
+
+    ver3 = mergeby(vertex3, [:response,]; name = name,
+        getid = g -> Ver3Id(para, g[1, :response], k = extK, t = extT)
+    )
+    return ver3
+end
