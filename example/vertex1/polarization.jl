26e77b43b0e8ecb11ae929034eca5fffed740cdb
Kun Chen <chenkun0228@gmail.com>
2022-02-08 20:54:33 -0500

    fix an example

--- example/vertex1/polarization.jl
+++ example/vertex1/polarization.jl
@@ -7,13 +7,53 @@ mpiexec -n julia polarization.jl
 
 Note that you may need to install julia MPI.jl following the link:
 https://juliaparallel.github.io/MPI.jl/stable/configuration/#Julia-wrapper-for-mpiexec
+
+With the parameters:
+const steps = 1e7 # MC steps of each block
+const Order = 3  #diagram order
+const dim = 3
+const rs = 1.0
+const beta = 25.0  # β*E_F
+const basic = Parameter.rydbergUnit(1 / beta, rs, dim, Λs = 1.0) # calculate all relevant parameters 
+
+We expect the outputs:
+Order 1
+                q/kF             polarMC                  error               exact
+            0.000000            0.097305 ±             0.000200           -0.048613
+            0.428571            0.095600 ±             0.000220           -0.047862
+            0.857143            0.091110 ±             0.000231           -0.045518
+            1.285714            0.082607 ±             0.000290           -0.041234
+            1.714286            0.067406 ±             0.000402           -0.033955
+            2.142857            0.037449 ±             0.000305           -0.018655
+            2.571429            0.022721 ±             0.000260           -0.011470
+            3.000000            0.015731 ±             0.000219           -0.008007
+Order 2
+                q/kF             polarMC                  error               exact
+            0.000000            0.022179 ±             0.000088
+            0.428571            0.021614 ±             0.000136
+            0.857143            0.019902 ±             0.000163
+            1.285714            0.017330 ±             0.000189
+            1.714286            0.012746 ±             0.000232
+            2.142857            0.003328 ±             0.000081
+            2.571429            0.001054 ±             0.000043
+            3.000000            0.000446 ±             0.000023
+Order 3
+                q/kF             polarMC                  error               exact
+            0.000000            0.007674 ±             0.000405
+            0.428571            0.007842 ±             0.000414
+            0.857143            0.007090 ±             0.000354
+            1.285714            0.005093 ±             0.000366
+            1.714286            0.003780 ±             0.000281
+            2.142857            0.003119 ±             0.000291
+            2.571429            0.001502 ±             0.000214
+            3.000000            0.000768 ±             0.000151
 """
 
 using Printf, LinearAlgebra
 using MCIntegration, FeynmanDiagram, ElectronGas, Lehmann #NumericalEFT packages
 
 ##################### parameters for 3D UEG ##############################
-const steps = 1e8 # MC steps of each block
+const steps = 1e7 # MC steps of each block
 const Order = 3  #diagram order
 const dim = 3
 const rs = 1.0
@@ -38,8 +78,8 @@ const polar = [mergeby(Parquet.polarization(para[i])).diagram[1] for i in 1:Orde
 const diag = [ExprTree.build(polar[o]) for o in 1:Order]    #experssion tree representation of diagrams 
 
 ##################### propagator and interaction evaluation ##############
-function eval(id::GreenId, K, varT)
-    τin, τout = varT[id.extT[1]], varT[id.extT[2]]
+function eval(id::GreenId, K, extT, varT)
+    τin, τout = varT[extT[1]], varT[extT[2]]
     ϵ = dot(K, K) / (2me) - μ
     τ = τout - τin
     if τ ≈ 0.0
@@ -49,7 +89,7 @@ function eval(id::GreenId, K, varT)
     end
 end
 
-eval(id::InteractionId, K, varT) = (basic.e0)^2 / basic.ϵ0 / (dot(K, K) + basic.Λs)
+eval(id::InteractionId, K, extT, varT) = (basic.e0)^2 / basic.ϵ0 / (dot(K, K) + basic.Λs)
 
 ################### interface to MC #########################################
 function integrand(config)
