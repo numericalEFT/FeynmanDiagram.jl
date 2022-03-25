e18781e1f1c4957aed630a3abc3bdf2854fd1d82
Kun Chen <chenkun0228@gmail.com>
2022-03-25 15:12:50 -0400

    chain4 still has bug

--- example/strong_coupling_expansion/chain4.jl
+++ example/strong_coupling_expansion/chain4.jl
@@ -24,52 +24,60 @@ const O = MCIntegration.Discrete(1, 2)
 O.data[1] = O.data[2] = O.data[3] = O.data[4] = 1
 const para = GenericPara(diagType = Ver4Diag, innerLoopNum = 1, hasTau = true)
 
-const n12 = BareHoppingId(para, (1, 1), (1, 1), (1, 2))
-const n11 = BareHoppingId(para, (1, 1), (1, 1), (1, 1))
+No = 9
+bond(o, rin, rout) = BareHoppingId(para, (o, o), (o, o), (rin, rout))
+const hop = Array{BareHoppingId,3}(undef, No, 4, 4)
+for o in 2:No
+    hop[o, 1, 2] = bond(o, 1, 2)
+    hop[o, 2, 3] = bond(o, 2, 3)
+    hop[o, 3, 4] = bond(o, 3, 4)
+    hop[o, 4, 1] = bond(o, 4, 1)
+    hop[o, 2, 1] = bond(o, 2, 1)
+    hop[o, 3, 2] = bond(o, 3, 2)
+    hop[o, 4, 3] = bond(o, 4, 3)
+    hop[o, 1, 4] = bond(o, 1, 4)
+end
+const n11 = bond(1, 1, 1)
+const n12 = bond(1, 1, 2)
+const n13 = bond(1, 1, 3)
+const n14 = bond(1, 1, 4)
 
-const h2_12 = BareHoppingId(para, (2, 2), (2, 2), (1, 2))
-const h2_21 = BareHoppingId(para, (2, 2), (2, 2), (2, 1))
-const h3_12 = BareHoppingId(para, (3, 3), (3, 3), (1, 2))
-const h3_21 = BareHoppingId(para, (3, 3), (3, 3), (2, 1))
-const h4_12 = BareHoppingId(para, (4, 4), (4, 4), (1, 2))
-const h4_21 = BareHoppingId(para, (4, 4), (4, 4), (2, 1))
-const h5_12 = BareHoppingId(para, (5, 5), (5, 5), (1, 2))
-const h5_21 = BareHoppingId(para, (5, 5), (5, 5), (2, 1))
-const h6_12 = BareHoppingId(para, (6, 6), (6, 6), (1, 2))
-const h6_21 = BareHoppingId(para, (6, 6), (6, 6), (2, 1))
-const h7_12 = BareHoppingId(para, (7, 7), (7, 7), (1, 2))
-const h7_21 = BareHoppingId(para, (7, 7), (7, 7), (2, 1))
-const h8_12 = BareHoppingId(para, (8, 8), (8, 8), (1, 2))
-const h8_21 = BareHoppingId(para, (8, 8), (8, 8), (2, 1))
-const h9_12 = BareHoppingId(para, (9, 9), (9, 9), (1, 2))
-const h9_21 = BareHoppingId(para, (9, 9), (9, 9), (2, 1))
-
-const diag1_12 = SCE.connectedGreen(para, [n12, h2_21])
+const diag1_12 = SCE.connectedGreen(para, [n12, hop[2, 2, 1]])
+# const diag1_14 = SCE.connectedGreen(para, [n14, hop[2, 4, 1]])
 const tree1 = ExprTree.build([diag1_12,], false)
 
-const diag2_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21])
-const tree2 = ExprTree.build([diag2_11,], false)
-plot_tree(diag2_11)
-exit(0)
+const diag2_11_1 = SCE.connectedGreen(para, [n11, hop[2, 1, 2], hop[2, 2, 1]])
+const diag2_11_2 = SCE.connectedGreen(para, [n11, hop[2, 1, 4], hop[2, 4, 1]])
+const tree2 = ExprTree.build([diag2_11_1, diag2_11_2,], false)
 
-const diag3_12 = SCE.connectedGreen(para, [n12, h2_12, h3_21, h4_21])
-const tree3 = ExprTree.build([diag3_12,], false)
+println([n13, hop[2, 1, 2], hop[2, 2, 3]])
+const diag2_13_1 = SCE.connectedGreen(para, [n13, hop[2, 1, 2], hop[2, 2, 3]])
+const diag2_13_2 = SCE.connectedGreen(para, [n13, hop[2, 1, 4], hop[2, 4, 3]])
+const tree3 = ExprTree.build([diag2_13_1, diag2_13_2,], false)
+
+const diag3_12_1 = SCE.connectedGreen(para, [n12, hop[2, 1, 2], hop[3, 2, 1], hop[4, 2, 1]])
+const diag3_12_2 = SCE.connectedGreen(para, [n12, hop[2, 2, 3], hop[3, 3, 4], hop[4, 4, 1]])
+const tree4 = ExprTree.build([diag3_12_1, diag3_12_2,], false)
 
 # const diag4_11 = SCE.connectedGreen(para, [n11, h2_12, h3_12, h4_21, h5_21])
-const diag4_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21, h4_21, h5_12])
-const tree4 = ExprTree.build([diag4_11,], false)
+const diag4_11_1 = SCE.connectedGreen(para, [n11, hop[2, 1, 2], hop[3, 2, 1], hop[4, 2, 1], hop[5, 1, 2]])
+const diag4_11_2 = SCE.connectedGreen(para, [n11, hop[2, 1, 2], hop[3, 2, 3], hop[4, 3, 2], hop[5, 2, 1]])
+const diag4_11_3 = SCE.connectedGreen(para, [n11, hop[2, 1, 4], hop[3, 4, 3], hop[4, 3, 4], hop[5, 4, 1]])
+const diag4_11_4 = SCE.connectedGreen(para, [n11, hop[2, 1, 2], hop[3, 2, 3], hop[4, 3, 4], hop[5, 4, 1]])
+const diag4_11_5 = SCE.connectedGreen(para, [n11, hop[2, 1, 4], hop[3, 4, 3], hop[4, 3, 2], hop[5, 2, 1]])
+const tree5 = ExprTree.build([diag4_11_1, diag4_11_2, diag4_11_3, diag4_11_4, diag4_11_5,], false)
 
-const diag5_12 = SCE.connectedGreen(para, [n12, h2_12, h3_21, h4_21, h5_12, h6_21])
-const tree5 = ExprTree.build([diag5_12,], false)
+# const diag5_12 = SCE.connectedGreen(para, [n12, h2_12, h3_21, h4_21, h5_12, h6_21])
+# const tree5 = ExprTree.build([diag5_12,], false)
 
-const diag6_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12])
-const tree6 = ExprTree.build([diag6_11,], false)
+# const diag6_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12])
+# const tree6 = ExprTree.build([diag6_11,], false)
 
-const diag7_12 = SCE.connectedGreen(para, [n12, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12, h8_21])
-const tree7 = ExprTree.build([diag7_12,], false)
+# const diag7_12 = SCE.connectedGreen(para, [n12, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12, h8_21])
+# const tree7 = ExprTree.build([diag7_12,], false)
 
-const diag8_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12, h8_21, h9_12])
-const tree8 = ExprTree.build([diag8_11,], false)
+# const diag8_11 = SCE.connectedGreen(para, [n11, h2_12, h3_21, h4_21, h5_12, h6_21, h7_12, h8_21, h9_12])
+# const tree8 = ExprTree.build([diag8_11,], false)
 # const tree12 = ExprTree.build([diag12,], false)
 # plot_tree(diag1_12)
 # plot_tree(diag2_11)
@@ -88,7 +96,7 @@ const tree8 = ExprTree.build([diag8_11,], false)
 # const diag2_1212 = SCE.connectedGreen(para, [h1_12, h2_21, h3_12, h4_21])
 # # const tree2 = ExprTree.build([diag2_1122, diag2_1221, diag2_1212], false)
 # const tree2 = ExprTree.build([diag2_1122], false)
-const tree = [tree1, tree2, tree3, tree4, tree5, tree6, tree7, tree8]
+const tree = [tree1, tree2, tree3, tree4, tree5]
 # const tree = [tree1,]
 # plot_tree(diag2_1122)
 # plot_tree(diag2_1221)
@@ -155,7 +163,7 @@ function integrand(config)
         return tree[2][1]   # additional factor from 1/n! Then there are two copies of bonds (1->2) (2->1) and (2->1) (1->2)
     elseif config.curr == 3
         ExprTree.evalNaive!(tree[3], nothing, T.data)
-        return tree[3][1] / 10.0
+        return tree[3][1]
     elseif config.curr == 4
         ExprTree.evalNaive!(tree[4], nothing, T.data)
         return tree[4][1] / 10.0
@@ -186,8 +194,8 @@ function run(totalStep)
     # dof = [[4, 4],]
     # observable = zeros(1)
     # dof = [[2, 2], [4, 4]]
-    dof = [[2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9]]
-    observable = zeros(8)
+    dof = [[2, 2], [3, 3], [3, 3], [4, 4], [5, 5]]
+    observable = zeros(5)
 
     # g = Green.GreenN(paraAtom.m, [0.0, extT[1]], [UP, UP])
     # g2 = Green.Gn(paraAtom.m, g)
