b9a1e1a841ab25a97200c4a93171e43e01d8210e
Kun Chen <chenkun0228@gmail.com>
2022-03-28 12:51:03 -0400

    ver4.jl now correctly reproduce the RPA dynamic interaction one-loop vertex-4 diagrams

--- example/vertex4/ver4.jl
+++ example/vertex4/ver4.jl
@@ -12,7 +12,7 @@ include("parameter.jl")
 include("interaction.jl")
 
 
-const steps = 1e6
+const steps = 1e5
 const Order = 1
 const isF = false
 
@@ -34,54 +34,50 @@ KinL = KoutL = [1.0, 0, 0]
 KinR = KoutR = [0, 1.0, 0]
 legK = [KinL, KoutL, KinR, KoutR]
 
-diagPara(order) = GenericPara(diagType = Ver4Diag, innerLoopNum = order, hasTau = true, loopDim = dim, spin = spin, firstLoopIdx = 3,
-    interaction = [FeynmanDiagram.Interaction(ChargeCharge, [
+diagPara(order) = GenericPara(diagType=Ver4Diag, innerLoopNum=order, hasTau=true, loopDim=dim, spin=spin, firstLoopIdx=3,
+    interaction=[FeynmanDiagram.Interaction(ChargeCharge, [
         Instant,
         Dynamic
     ]),],  #instant charge-charge interaction
-    filter = [
+    filter=[
         Girreducible,
         Proper,   #one interaction irreduble diagrams or not
         NoBubble, #allow the bubble diagram or not
     ],
-    transferLoop = KinL - KoutL
+    transferLoop=KinL - KoutL
 )
 
 println("Build the diagrams into an experssion tree ...")
 const para = [diagPara(o) for o in 1:Order]
-ver4 = [Parquet.vertex4(para[i], legK, [PHEr,]) for i in 1:Order]   #diagram of different orders
+ver4 = [Parquet.vertex4(para[i], legK, [PHr,]) for i in 1:Order]   #diagram of different orders
+# ver4 = [Parquet.vertex4(para[i], legK, [PHEr,]) for i in 1:Order]   #diagram of different orders
+# ver4 = [Parquet.vertex4(para[i], legK, [PPr,]) for i in 1:Order]   #diagram of different orders
 #different order has different set of K, T variables, thus must have different exprtrees
 println(ver4)
-"""
-DataFrame[6×5 DataFrame
- Row │ response  type     extT          diagram                            hash  
-     │ Any       Any      Any           Diagram…                           Int64 
-─────┼───────────────────────────────────────────────────────────────────────────
-   1 │ UpUp      Dynamic  (1, 1, 2, 2)  ↑↑Dyn,t(1, 1, 2, 2)=0.0=⨁ (17, 2…     81
-   2 │ UpUp      Dynamic  (1, 2, 1, 2)  ↑↑x↑↑ → PPr, PPr ↑↑Dyn,t(1, 2, 1…     71
-   3 │ UpUp      Dynamic  (1, 2, 2, 1)  ↑↑Dyn,t(1, 2, 2, 1)=0.0=⨁ (43, 5…     82
-   4 │ UpDown    Dynamic  (1, 1, 2, 2)  ↑↓Dyn,t(1, 1, 2, 2)=0.0=⨁ (20, 2…     83
-   5 │ UpDown    Dynamic  (1, 2, 1, 2)  ↑↓Dyn,t(1, 2, 1, 2)=0.0=⨁ (74, 7…     84
-   6 │ UpDown    Dynamic  (1, 2, 2, 1)  ↑↓Dyn,t(1, 2, 2, 1)=0.0=⨁ (44, 4…     85]
-"""
+
+# plot_tree(ver4[1].diagram)
+# exit(0)
 # plot_tree(ver4uu[1][1])
 # plot_tree(ver4[1].diagram, maxdepth = 9)
 const diag = [ExprTree.build(ver4[o].diagram) for o in 1:Order]    #experssion tree representation of diagrams 
-const rootuu = [[idx for idx in d.root if d.nodePool.object[idx].para.response == UpUp] for d in diag]
-const rootud = [[idx for idx in d.root if d.nodePool.object[idx].para.response == UpDown] for d in diag]
-const extTuu = [[diag[ri].nodePool.object[idx].para.extT for idx in root] for (ri, root) in enumerate(rootuu)]
-const extTud = [[diag[ri].nodePool.object[idx].para.extT for idx in root] for (ri, root) in enumerate(rootud)]
-# println(rootuu)
-# println(extTuu)
-# println(rootud)
-# println(extTud)
+println(diag[1].root)
+println(length(diag[1].node.current))
+const rootuu = [[idx for idx in d.root if d.node.object[idx].para.response == UpUp] for d in diag] #select the diagram with upup
+const rootud = [[idx for idx in d.root if d.node.object[idx].para.response == UpDown] for d in diag] #select the diagram with updown
+#assign the external Tau to the corresponding diagrams
+const extTuu = [[diag[ri].node.object[idx].para.extT for idx in root] for (ri, root) in enumerate(rootuu)]
+const extTud = [[diag[ri].node.object[idx].para.extT for idx in root] for (ri, root) in enumerate(rootud)]
+println(rootuu)
+println(extTuu)
+println(rootud)
+println(extTud)
 # ExprTree.showTree(diag[1], rootuu[1][1])
 # ExprTree.showTree(diag[1], rootud[1][1])
 
 # exit(0)
 
 ##################### propagator and interaction evaluation ##############
-function eval(id::GreenId, K, varT)
+function eval(id::BareGreenId, K, extT, varT)
     τin, τout = varT[id.extT[1]], varT[id.extT[2]]
     ϵ = dot(K, K) / (2me) - μ
     τ = τout - τin
@@ -93,7 +89,7 @@ function eval(id::GreenId, K, varT)
 end
 
 # eval(id::InteractionId, K, varT) = e0^2 / ϵ0 / (dot(K, K) + mass2)
-function eval(id::InteractionId, K, varT)
+function eval(id::BareInteractionId, K, extT, varT)
     if id.type == Instant
         if id.para.interactionTauNum == 1
             return e0^2 / ϵ0 / (dot(K, K) + mass2)
@@ -126,7 +122,7 @@ function integrand(config)
     varK, varT = config.var[1], config.var[2]
     varK.data[:, 2] .= ExtK[x]
     # @assert varK.data[:, 1] ≈ [kF, 0.0, 0.0]
-    w = ExprTree.evalNaive!(diag[order], varK.data, varT.data, eval)
+    ExprTree.evalNaive!(diag[order], varK.data, varT.data, eval)
     # println(rootuu)
     # for (ri, root) in enumerate(rootuu)
     #     println(ri, ",  ", root)
@@ -135,12 +131,12 @@ function integrand(config)
     # @assert w[1] ≈ diag[order].nodePool.current[13]
     # @assert w[2] ≈ diag[order].nodePool.current[14]
     if !isempty(rootuu[order])
-        wuu = sum(diag[order].nodePool.current[root] * phase(varT, extTuu[order][ri]) for (ri, root) in enumerate(rootuu[order]))
+        wuu = sum(diag[order].node.current[root] * phase(varT, extTuu[order][ri]) for (ri, root) in enumerate(rootuu[order]))
     else
         wuu = 0.0
     end
     if !isempty(rootud[order])
-        wud = sum(diag[order].nodePool.current[root] * phase(varT, extTud[order][ri]) for (ri, root) in enumerate(rootud[order]))
+        wud = sum(diag[order].node.current[root] * phase(varT, extTud[order][ri]) for (ri, root) in enumerate(rootud[order]))
     else
         wud = 0.0
     end
@@ -162,7 +158,7 @@ function measure(config)
 end
 
 function MC()
-    K = MCIntegration.FermiK(dim, kF, 0.2 * kF, 10.0 * kF, offset = 2)
+    K = MCIntegration.FermiK(dim, kF, 0.2 * kF, 10.0 * kF, offset=2)
     K.data[:, 1] .= RefK
     T = MCIntegration.Tau(β, β / 2.0)
     # X = MCIntegration.Continuous([-1.0, 1.0], 0.2) #x=cos(θ)
@@ -177,7 +173,7 @@ function MC()
     obs = zeros(Nk, 2) # observable for the Fock diagram 
 
     config = MCIntegration.Configuration(steps, (K, T, X), dof, obs)
-    avg, std = MCIntegration.sample(config, integrand, measure; print = 0, Nblock = 16)
+    avg, std = MCIntegration.sample(config, integrand, measure; print=0, Nblock=16)
 
     function info(idx, di)
         return @sprintf("   %8.4f ±%8.4f", avg[idx, di], std[idx, di])
