97b2f8136d66edab7853b589122337002bba318c
Kun Chen <chenkun0228@gmail.com>
2022-03-29 10:24:03 -0400

    fix a bug minor bug on ver4_l error estimation

--- example/vertex4/ver4_l.jl
+++ example/vertex4/ver4_l.jl
@@ -95,7 +95,7 @@ function MC()
         end
         println("A ver4: ")
         for li in 1:N
-            @printf("%8.4f   %8.4f ±%8.4f\n", grid[li], (avg[li, 1] - avg[li, 2]) / 2, (std[li, 1] - std[li, 2]) / 2)
+            @printf("%8.4f   %8.4f ±%8.4f\n", grid[li], (avg[li, 1] - avg[li, 2]) / 2, (std[li, 1] + std[li, 2]) / 2)
         end
 
 
