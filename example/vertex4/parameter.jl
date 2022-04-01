448480703f0ded5300d0d2e03162e33fbfc191ec
Kun Chen <chenkun0228@gmail.com>
2022-04-01 17:46:55 -0400

    wip

--- example/vertex4/parameter.jl
+++ example/vertex4/parameter.jl
@@ -3,9 +3,9 @@ using StaticArrays
 using ElectronGas: Parameter
 
 const beta = 25.0
-const rs = 5.0
-const mass2 = 0.01
-const Fs = -0.6
+const rs = 5.3
+const mass2 = 0.001
+const Fs = -1.0
 const Fa = -0.0
 
 const para = Parameter.rydbergUnit(1.0 / beta, rs, 3, Λs=mass2)
