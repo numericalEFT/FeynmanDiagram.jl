5bc32748647ac0482c1479420abf1d940ce04737
Kun Chen <chenkun0228@gmail.com>
2022-06-07 22:39:56 -0400

    add the operation removeHatreeFock

--- src/diagram_tree/operation.jl
+++ src/diagram_tree/operation.jl
@@ -1,4 +1,4 @@
-function oneOrderHigher(diag::Diagram{W}, ::Type{Id}, subdiagram = []) where {W,Id}
+function oneOrderHigher(diag::Diagram{W}, ::Type{Id}, subdiagram=[]) where {W,Id}
     if diag.id isa PropagatorId && (diag.id isa Id) == false
         #for bare propagator, a derivative of different id vanishes
         return nothing
@@ -12,7 +12,7 @@ function oneOrderHigher(diag::Diagram{W}, ::Type{Id}, subdiagram = []) where {W,
         error("not implemented!")
     end
 
-    d = Diagram{W}(id, diag.operator, subdiagram; name = diag.name, factor = diag.factor, weight = diag.weight)
+    d = Diagram{W}(id, diag.operator, subdiagram; name=diag.name, factor=diag.factor, weight=diag.weight)
     return d
 end
 
@@ -72,7 +72,7 @@ function derivative(diags::Union{Diagram,Tuple,AbstractVector}, ::Type{ID}) wher
                     else
                         newid = deepcopy(id)
                         newid.order .= terms[1].id.order
-                        dual[d.hash] = Diagram{id.para.weightType}(newid, Sum(), terms, name = Symbol("$(d.name)'"))
+                        dual[d.hash] = Diagram{id.para.weightType}(newid, Sum(), terms, name=Symbol("$(d.name)'"))
                     end
                 else
                     error("not implemented!")
@@ -84,5 +84,65 @@ function derivative(diags::Union{Diagram,Tuple,AbstractVector}, ::Type{ID}) wher
         return dual[diags[1].hash]
     else
         return [dual[diag.hash] for diag in diags]
+    end
+end
+
+"""
+    function derivative(diags::Union{Diagram,Tuple,AbstractVector}, ::Type{ID}, order::Int) where {ID<:PropagatorId}
+    
+    Automatic differentiation derivative on the diagrams
+
+# Arguments
+- diags      : diagrams to take derivative
+- ID         : DiagramId to apply the differentiation
+- order::Int : derivative order
+"""
+function derivative(diags::Union{Diagram,Tuple,AbstractVector}, ::Type{ID}, order::Int) where {ID<:PropagatorId}
+    @assert order >= 0
+    if order == 0
+        return diags
+    end
+    result = diags
+    for o in 1:order
+        result = derivative(result, ID)
+    end
+    return result
+end
+
+"""
+    function removeHatreeFock!(diags::Union{Diagram,Tuple,AbstractVector})
+    
+    Remove the Hatree-Fock insertions that without any derivatives on the propagator and the interaction. 
+
+# Arguments
+- diags      : diagrams to remove the Fock insertion
+
+# Remarks
+    The operations removeHatreeFock! and taking derivatives doesn't commute with each other! 
+"""
+function removeHatreeFock!(diags::Union{Diagram,Tuple,AbstractVector})
+    single = false
+    if diags isa Diagram
+        diags = [diags,]
+        single = true
+    end
+    for diag in diags
+        for d in PreOrderDFS(diag)
+            # for subdiag in d.subdiagram
+            if d.id isa SigmaId
+                # println(d, " with ", d.id.para.innerLoopNum)
+                if isempty(d.id.order) || all(x -> x == 0, d.id.order) #eithr order is empty or a vector of zeros
+                    if d.id.para.innerLoopNum == 1
+                        d.factor = 0.0
+                    end
+                end
+            end
+            # end
+        end
+    end
+    if single
+        return diags[1]
+    else
+        return diags
     end
 end
