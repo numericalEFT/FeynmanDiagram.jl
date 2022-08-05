be116c05fb6e79f97a48a30c9c3d6431a4a27354
Kun Chen <chenkun0228@gmail.com>
2022-08-05 13:26:37 -0400

    add a note on hubbard atom test

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,23 @@
 using FeynmanDiagram
 using Documenter
 
-DocMeta.setdocmeta!(FeynmanDiagram, :DocTestSetup, :(using FeynmanDiagram); recursive = true)
+DocMeta.setdocmeta!(FeynmanDiagram, :DocTestSetup, :(using FeynmanDiagram); recursive=true)
 
 makedocs(;
-    modules = [FeynmanDiagram],
-    authors = "Kun Chen, Pengcheng Hou",
-    repo = "https://github.com/numericalEFT/FeynmanDiagram.jl/blob/{commit}{path}#{line}",
-    sitename = "FeynmanDiagram.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://numericaleft.github.io/FeynmanDiagram.jl",
-        assets = String[]
+    modules=[FeynmanDiagram],
+    authors="Kun Chen, Pengcheng Hou",
+    repo="https://github.com/numericalEFT/FeynmanDiagram.jl/blob/{commit}{path}#{line}",
+    sitename="FeynmanDiagram.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://numericaleft.github.io/FeynmanDiagram.jl",
+        assets=String[]
     ),
-    pages = [
+    pages=[
         "Home" => "index.md",
         "Manual" => [
             "manual/interaction.md",
+            "manual/hubbard_atom.md"
         ],
         "API reference" => Any[
             "lib/parquet.md",
@@ -27,5 +28,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo = "github.com/numericalEFT/FeynmanDiagram.jl"
+    repo="github.com/numericalEFT/FeynmanDiagram.jl"
 )
