8c8cde5cb550bfbf64b4b77956b95e9d62d3b763
Kun Chen <chenkun0228@gmail.com>
2022-08-08 23:23:12 -0400

    more robust mergeby

--- src/FeynmanDiagram.jl
+++ src/FeynmanDiagram.jl
@@ -102,6 +102,11 @@ if ccall(:jl_generating_output, Cint, ()) == 1   # if we're precompiling the pac
         para = DiagParaF64(diagType=Ver4Diag, innerLoopNum=2, hasTau=true)
         # ver4 = Parquet.vertex4(para)  # this will force precompilation
         ver4 = Parquet.build(para)  # this will force precompilation
+
+        mergeby(ver4, [:response])
+        mergeby(ver4.diagram)
+        mergeby(ver4.diagram, [:response]; idkey=[:extT, :response])
+
         para = DiagParaF64(diagType=SigmaDiag, innerLoopNum=2, hasTau=true)
         Parquet.build(para)  # this will force precompilation
         para = DiagParaF64(diagType=GreenDiag, innerLoopNum=2, hasTau=true)
@@ -113,9 +118,9 @@ if ccall(:jl_generating_output, Cint, ()) == 1   # if we're precompiling the pac
         # Parquet.vertex3(para)  # this will force precompilation
         Parquet.build(para)  # this will force precompilation
 
-        # DiagTree.removeHatreeFock!(ver4.diagram)
-        # DiagTree.derivative(ver4.diagram, BareGreenId)
-        # DiagTree.derivative(ver4.diagram, BareInteractionId)
+        DiagTree.removeHatreeFock!(ver4.diagram)
+        DiagTree.derivative(ver4.diagram, BareGreenId)
+        DiagTree.derivative(ver4.diagram, BareInteractionId)
         # DiagTree.removeHatreeFock!(ver4.diagram)
         ExprTree.build(ver4.diagram)
     end
