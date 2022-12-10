44b262981c7bb1f6139948e33bc4f7ddff68cc7f
dcerkoney <dcerkoney@gmail.com>
2022-12-10 03:44:55 -0500

    Fix subgraph factors and update Base overloads

--- src/computational_graph/graph.jl
+++ src/computational_graph/graph.jl
@@ -9,9 +9,6 @@ Base.show(io::IO, o::AbstractOperator) = print(io, typeof(o))
 Base.show(io::IO, ::Type{Sum}) = print(io, "⨁")
 Base.show(io::IO, ::Type{Prod}) = print(io, "Ⓧ")
 
-# """Type alias for a directed graph edge e = (a₁⁺, a₂⁻) from e[1] to e[2]."""
-# const EdgeType = Tuple{QuantumOperator,QuantumOperator}
-
 """
     mutable struct Graph{F,W}
     
@@ -26,7 +23,7 @@ Base.show(io::IO, ::Type{Prod}) = print(io, "Ⓧ")
 - `vertices::Vector{OperatorProduct}`  vertices of the diagram. Each index is composited by the product of quantum operators.
 - `topology::Vector{Vector{Int}}` topology of the diagram. Each Vector{Int} stores vertices' index connected with each other (as a propagator). 
 - `subgraphs::Vector{Graph{F,W}}`  vector of sub-diagrams 
-- `subgraph_factors::Vector{F}`  scalar multiplicative factors for each subdiagram
+- `subgraph_factors::Vector{F}`  scalar multiplicative factors associated with each subdiagram
 - `operator::DataType`  node operation, support Sum and Prod
 - `factor::F`  total scalar multiplicative factor for the diagram
 - `weight::W`  weight of the diagram
@@ -71,22 +68,23 @@ mutable struct Graph{F,W} # Graph
     - `external`  index of external vertices in terms of QuantumOperators, empty by default
     - `topology` topology of the diagram
     - `subgraphs`  vector of sub-diagrams 
-    - `subgraph_factors::Vector{F}`  scalar multiplicative factors for each subdiagram
+    - `subgraph_factors::Vector{F}`  scalar multiplicative factors associated with each subdiagram
     - `name`  name of the diagram
     - `type`  type of the diagram
     - `operator::DataType`  node operation, Sum, Prod, etc.
     - `orders`  orders of the diagram
     - `ftype`  typeof(factor)
     - `wtype`  typeof(weight)
-    - `factor::F`  total scalar multiplicative factor for the diagram
+    - `factor::F`  overall scalar multiplicative factor for this diagram (e.g., permutation sign)
     - `weight`  weight of the diagram
     """
-    function Graph(vertices::AbstractVector; external=[], subgraphs=[], subgraph_factors=getproperty.(subgraphs, :factor),
+    function Graph(vertices::AbstractVector; external=[], subgraphs=[], subgraph_factors=one.(eachindex(subgraphs)),
         topology=[], name="", type=:generic, operator::AbstractOperator=Sum(), orders=zeros(Int, 16),
         ftype=_dtype.factor, wtype=_dtype.weight, factor=one(ftype), weight=zero(wtype)
     )
         vertices = [OperatorProduct(v) for v in vertices]
-        return new{ftype,wtype}(uid(), name, type, orders, external, vertices, topology, subgraphs, subgraph_factors, typeof(operator), factor, weight)
+        return new{ftype,wtype}(uid(), name, type, orders, external, vertices, topology,
+            subgraphs, subgraph_factors, typeof(operator), factor, weight)
     end
 end
 
@@ -145,13 +143,6 @@ is_internal(g::Graph, i::Int) = (i in g.external) == false
 """
 external(g::Graph) = OperatorProduct(g.vertices)[g.external]
 
-# """
-#     function internal_vertices(g::Graph)
-
-#     Return all internal vertices (::Vector{OperatorProduct}) of Graph `g`.
-# """
-# internal_vertices(g::Graph) = g.vertices[setdiff(eachindex(g.vertices), g.external)]
-
 """
     function vertices(g::Graph)
 
@@ -170,72 +161,68 @@ function connectivity(g::Graph)
 end
 
 function Base.:*(g1::Graph{F,W}, c2::C) where {F,W,C}
-    return Graph(g1.vertices; external=g1.external, type=g1.type, topology=g1.topology,
-        subgraphs=[g1,], operator=Prod(), ftype=F, wtype=W, factor=F(c2) * g1.factor)
+    g = Graph(g1.vertices; external=g1.external, type=g1.type, topology=g1.topology,
+        subgraphs=[g1,], subgraph_factors=[F(c2),], operator=Prod(), ftype=F, wtype=W)
+    # Merge multiplicative chains
+    if g1.operator == Prod && length(g1.subgraph_factors) == 1
+        g.subgraph_factors[1] *= g1.subgraph_factors[1]
+        g.subgraphs = g1.subgraphs
+    end
+    return g
 end
 
 function Base.:*(c1::C, g2::Graph{F,W}) where {F,W,C}
-    return Graph(g2.vertices; external=g2.external, type=g2.type, topology=g2.topology,
-        subgraphs=[g2,], operator=Prod(), ftype=F, wtype=W, factor=F(c1) * g2.factor)
+    g = Graph(g2.vertices; external=g2.external, type=g2.type, topology=g2.topology,
+        subgraphs=[g2,], subgraph_factors=[F(c1),], operator=Prod(), ftype=F, wtype=W)
+    # Merge multiplicative chains
+    if g2.operator == Prod && length(g2.subgraph_factors) == 1
+        g.subgraph_factors[1] *= g2.subgraph_factors[1]
+        g.subgraphs = g2.subgraphs
+    end
+    return g
+end
+
+"""Returns a graph representing the linear combination `c1*g1 + c2*g2`."""
+function linear_combination(g1::Graph{F,W}, g2::Graph{F,W}, c1::C, c2::C) where {F,W,C}
+    # TODO: more check
+    @assert g1.type == g2.type "g1 and g2 are not of the same type."
+    @assert g1.orders == g2.orders "g1 and g2 have different orders."
+    @assert Set(vertices(g1)) == Set(vertices(g2)) "g1 and g2 have different vertices."
+    @assert Set(external(g1)) == Set(external(g2)) "g1 and g2 have different external vertices."
+    return Graph(g1.vertices; external=g1.external, type=g1.type, subgraphs=[g1, g2],
+        subgraph_factors=[F(c1), F(c2)], operator=Sum(), ftype=F, wtype=W)
+end
+
+"""
+Given a vector `graphs` of graphs each with the same type and external/internal
+vertices and an equally-sized vector `constants` of constants, returns a new
+graph representing the linear combination ⟨`graphs`, `constants`⟩.
+"""
+function linear_combination(graphs::Vector{Graph{F,W}}, constants::Vector{C}) where {F,W,C}
+    # TODO: more check
+    @assert allequal(getproperty.(graphs, :type)) "Graphs are not all of the same type."
+    @assert allequal(getproperty.(graphs, :orders)) "Graphs do not all have the same order."
+    @assert allequal(Set.(vertices.(graphs))) "Graphs do not share the same set of vertices."
+    @assert allequal(Set.(external.(graphs))) "Graphs do not share the same set of external vertices."
+    g1 = graphs[1]
+    return Graph(g1.vertices; external=g1.external, type=g1.type, subgraphs=graphs,
+        subgraph_factors=constants, operator=Sum(), ftype=F, wtype=W)
 end
 
 function Base.:+(g1::Graph{F,W}, g2::Graph{F,W}) where {F,W}
-    @assert g1.type == g2.type "g1 and g2 are not of the same type."
-    # TODO: more check
-    @assert Set(vertices(g1)) == Set(vertices(g2)) "g1 and g2 have different vertices."
-    @assert Set(external(g1)) == Set(external(g2)) "g1 and g2 have different external vertices."
-    @assert g1.orders == g2.orders "g1 and g2 have different orders."
-
-    return Graph(g1.vertices; external=g1.external, type=g1.type, subgraphs=[g1, g2], operator=Sum(), ftype=F, wtype=W)
+    return linear_combination(g1, g2, F(1), F(1))
 end
 
 function Base.:-(g1::Graph{F,W}, g2::Graph{F,W}) where {F,W}
-    return g1 + (-F(1)) * g2
+    return linear_combination(g1, g2, F(1), F(-1))
 end
 
-# """
-#     function feynman_diagram(vertices::Vector{OperatorProduct}, contractions::Vector{Int};
-#         external=[], factor=one(_dtype.factor), weight=zero(_dtype.weight), name="", type=:generic)
-
-#     Create a Graph representing feynman diagram from all vertices and Wick contractions.
-
-# # Arguments:
-# - `vertices::Vector{OperatorProduct}`  vertices of the diagram
-# - `contractions::Vector{Int}`  contraction-index vector respresnting Wick contractions
-# - `external`  index of external vertices
-# - `factor`  scalar multiplicative factor for the diagram
-# - `weight`  weight of the diagram
-# - `name`  name of the diagram
-# - `type`  type of the diagram
-
-# # Example:
-# ```julia-repl
-# julia> g = feynman_diagram([𝑓⁺(1)𝑓⁻(2)𝜙(3), 𝑓⁺(4)𝑓⁻(5)𝜙(6)], [1, 2, 3, 2, 1, 3])
-# 1: generic graph from f⁺(1)f⁻(2)ϕ(3)|f⁺(4)f⁻(5)ϕ(6)
-
-# julia> g.subgraphs
-# 3-element Vector{Graph{Float64, Float64}}:
-#  2: propagtor graph from f⁺(1)f⁻(5)
-#  3: propagtor graph from f⁻(2)f⁺(4)
-#  4: propagtor graph from ϕ(3)ϕ(6)
-# ```
-# """
-# function feynman_diagram(vertices::Vector{OperatorProduct}, contractions::Vector{Int};
-#     external=[], factor=one(_dtype.factor), weight=zero(_dtype.weight), name="", type=:generic)
-
-#     contraction_sign, topology, edges = contractions_to_edges(vertices, contractions)
-#     g = Graph(vertices; external=external, topology=topology, name=name, type=type, operator=Prod(),
-#         factor=factor * contraction_sign, weight=weight)
-#     for edge in edges
-#         push!(g.subgraphs, propagator(reduce(*, edge)))
-#     end
-#     return g
+# function Base.:+(g1::Graph{F,W}, g2::Graph{F,W}) where {F,W}
+#     return linear_combination([g1, g2], [F(1), F(1)])
 # end
-# function feynman_diagram(graphs::Vector{Graph{F,W}}, contractions::Vector{Int};
-#     external=[], factor=one(_dtype.factor), weight=zero(_dtype.weight), name="", type=:generic) where {F,W}
 
-#     vertices = [v for g in graphs for v in external_vertices(g)]
-#     return feynman_diagram(vertices, contractions; external=external, factor=factor, weight=weight, name=name, type=type)
+# function Base.:-(g1::Graph{F,W}, g2::Graph{F,W}) where {F,W}
+#     return linear_combination([g1, g2], [F(1), F(-1)])
 # end
 
 """
@@ -248,7 +235,7 @@ end
 - `vertices::Vector{OperatorProduct}`  vertices of the diagram
 - `topology::Vector{Vector{Int}}` topology of the diagram. Each Vector{Int} stores vertices' index connected with each other (as a propagator). 
 - `external`  index of external vertices
-- `factor`  scalar multiplicative factor for the diagram
+- `factor::F`  overall scalar multiplicative factor for this diagram (e.g., permutation sign)
 - `weight`  weight of the diagram
 - `name`  name of the diagram
 - `type`  type of the diagram
@@ -292,80 +279,6 @@ function feynman_diagram(vertices::Vector{OperatorProduct}, topology::Vector{Vec
     end
     return g
 end
-# function feynman_diagram(graphs::Vector{Graph{F,W}}, topology::Vector{Vector{Int}};
-#     external=[], factor=one(_dtype.factor), weight=zero(_dtype.weight), name="", type=:generic) where {F,W}
-
-#     vertices = reduce(*, [v for g in graphs for v in external_vertices(g)])
-#     return feynman_diagram(vertices, topology; external=external, factor=factor, weight=weight, name=name, type=type)
-# end
-
-# """
-#     function contractions_to_edges(vertices::Vector{OperatorProduct}, contractions::Vector{Int})
-
-#     Converts a list of Wick contractions associated with a list of vertices
-#     to a list of edges e = (i, j) directed from `operators[i]` to `operators[j]`, where
-#     `operators` is a flattened list of operators associated with the specified `vertices`.
-
-# # Example: 
-# ```julia-repl
-# julia> vertices = [𝑏⁺(1)𝑓⁺(2)𝜙(3), 𝑓⁻(4)𝑓⁻(5), 𝑏⁻(6)𝑓⁺(7)𝜙(8)];
-
-# julia> edges, sign = contractions_to_edges(vertices, [1, 2, 3, 2, 4, 1, 4, 3])
-# (Tuple{QuantumOperator, QuantumOperator}[(b⁺(1), b⁻(6)), (f⁺(2), f⁻(4)), (ϕ(3), ϕ(8)), (f⁻(5), f⁺(7))], 1)
-# ```
-# - flattened fermionic edges: [2, 4, 5, 7]
-# - sign = parity([1, 2, 3, 4]) = 1
-# """
-# function contractions_to_edges(vertices::Vector{OperatorProduct}, contractions::Vector{Int})
-#     #TODO: only works for weak-coupling expansion with Wick's theorem for now.
-#     # Obtain the flattened list of non-composite operators
-#     operators = [o for v in vertices for o in v.operators]
-#     # Filter some illegal contractions
-#     is_invalid =
-#         isodd(length(contractions)) ||
-#         # isodd(length(unique(contractions))) ||
-#         length(operators) != length(contractions)
-#     if is_invalid
-#         throw(
-#             ArgumentError(
-#                 "Input $contractions does not specify a legal set of Wick contractions",
-#             ),
-#         )
-#     end
-#     # Loop over operators and pair
-#     next_pairing = 1
-#     edges = Vector{EdgeType}()
-#     topology = Vector{Int}[]
-#     permutation = Int[]
-
-#     for (i, wick_i) in enumerate(contractions)
-#         if i < next_pairing
-#             continue  # Operator already paired
-#         end
-#         for (js, wick_j) in enumerate(contractions[(i+1):end])
-#             j = i + js  # Iterating for (j > i)
-#             if j < next_pairing
-#                 continue  # Operator already paired
-#             end
-#             if wick_i == wick_j
-#                 @debug "Found Wick contraction #$wick_i, adding edge between operators $i and $j"
-#                 @assert operators[j]'.operator == operators[i].operator
-#                 isfermionic(operators[j]) && append!(permutation, [i, j])
-#                 push!(edges, (operators[i], operators[j]))
-#                 push!(topology, [i, j])
-#                 # Move on to next pair
-#                 next_pairing += 1
-#                 break
-#             end
-#         end
-#     end
-#     # Deduce the parity of the contraction
-#     # Get the permutation parity for the flattened list of fermionic edges, e.g.,
-#     # permutation = [1, 5, 4, 8, 7, 6] => P = (1 3 2 6 5 4) => sign = +1
-#     sign = isempty(permutation) ? 1 : parity(sortperm(permutation))
-
-#     return sign, topology, edges
-# end
 
 """
     function propagator(ops::OperatorProduct;
