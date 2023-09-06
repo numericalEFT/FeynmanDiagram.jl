73538aec3a4921ebd35e89333351a311baeb59e5
Kun Chen <chenkun0228@gmail.com>
2023-09-06 16:11:03 -0400

    working on free energy diagrams

--- src/frontend/GV_diagrams/main_freeenergy.py
+++ src/frontend/GV_diagrams/main_freeenergy.py
@@ -1,11 +1,10 @@
 from free_energy import *
-from polar import *
 import copy
 import sys
 
 
 def Generate(Order, VerOrder, SigmaOrder, SPIN):
-    LnZOrder = Order-1
+    LnZOrder = Order
     DiagFile = "./Diagram/HugenDiag{0}.diag".format(LnZOrder)
     LnZ = free_energy(LnZOrder)
     # Load pre-generated lnZ diagrams
@@ -16,23 +15,29 @@ def Generate(Order, VerOrder, SigmaOrder, SPIN):
     print red("\nThe optimimal LnZ diagrams:")
     OptLnZHugenDiagList = LnZ.OptimizeLoopBasis()
 
-    Polar = polar(Order)
-
-    UniqueUnLabelDiagList = []
-
     for d in OptLnZHugenDiagList:
         print "\n============================================================="
         print blue("Processing LnZ diagram: {0} with SymFactor: {1}".format(
             d.GetPermu(), d.SymFactor))
 
+    print "Save diagrams ..."
+
+    fname = "./groups_free_energy/{0}{1}_{2}_{3}.diag".format(
+        "FreeEnergy", Order, VerOrder, SigmaOrder)
+
+    with open(fname, "w") as f:
+        str_polar = LnZ.ToString(OptLnZHugenDiagList, VerOrder, SigmaOrder, SPIN)
+        if not(str_polar is None):
+            f.write(str_polar)
+
 
 if __name__ == "__main__":
     # print "Input Diagram Order: "
     # Order = int(sys.argv[1])
-    Order = 2
+    Order = 3
     SPIN = 2
     # for o in range(2, Order+1):
-    for o in range(2, Order+1):
+    for o in range(1, Order):
         for v in range(0, Order):
             # for g in range(0, (Order-1)/2+1):
             for g in range(0, Order):
