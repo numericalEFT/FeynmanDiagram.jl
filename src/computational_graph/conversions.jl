6db6d5bccd77732aa5899449ecc9cadbd2b63a3d
dcerkoney <dcerkoney@gmail.com>
2023-10-24 04:27:03 -0400

    Update conversions

--- src/computational_graph/conversions.jl
+++ src/computational_graph/conversions.jl
@@ -23,9 +23,12 @@ end
 Base.promote_rule(::Type{Graph{F,W}}, ::Type{FeynmanGraph{F,W}}) where {F,W} = Graph{F,W}
 
 # Arithmetic operations for mixed Graph/FeynmanGraph types
-linear_combination(g1::Graph{F,W}, g2::FeynmanGraph{F,W}, c1, c2) where {F,W} = linear_combination(Base.promote(g1, g2)..., c1, c2)
-linear_combination(g1::FeynmanGraph{F,W}, g2::Graph{F,W}, c1, c2) where {F,W} = linear_combination(Base.promote(g1, g2)..., c1, c2)
-linear_combination(graphs::Vector{Union{Graph{F,W},FeynmanGraph{F,W}}}, constants::AbstractVector) where {F,W} = linear_combination(Base.promote(graphs)..., constants)
+linear_combination(g1::Graph{F,W}, g2::FeynmanGraph{F,W}, c1=F(1), c2=F(1)) where {F,W} = linear_combination(Base.promote(g1, g2)..., c1, c2)
+linear_combination(g1::FeynmanGraph{F,W}, g2::Graph{F,W}, c1=F(1), c2=F(1)) where {F,W} = linear_combination(Base.promote(g1, g2)..., c1, c2)
+linear_combination(graphs::Vector{Union{Graph{F,W},FeynmanGraph{F,W}}}, constants::AbstractVector=ones(F, length(graphs))) where {F,W} = linear_combination(Base.promote(graphs)..., constants)
+multi_product(g1::Graph{F,W}, g2::FeynmanGraph{F,W}, c1=F(1), c2=F(1)) where {F,W} = multi_product(Base.promote(g1, g2)..., c1, c2)
+multi_product(g1::FeynmanGraph{F,W}, g2::Graph{F,W}, c1=F(1), c2=F(1)) where {F,W} = multi_product(Base.promote(g1, g2)..., c1, c2)
+multi_product(graphs::Vector{Union{Graph{F,W},FeynmanGraph{F,W}}}, constants::AbstractVector=ones(F, length(graphs))) where {F,W} = multi_product(Base.promote(graphs)..., constants)
 Base.:*(g1::Graph, g2::FeynmanGraph) = error("Multiplication of Feynman graphs is not well defined!")
 Base.:*(g1::FeynmanGraph, g2::Graph) = error("Multiplication of Feynman graphs is not well defined!")
 Base.:+(g1::Graph{F,W}, g2::FeynmanGraph{F,W}) where {F,W} = Base.:+(Base.promote(g1, g2)...)
