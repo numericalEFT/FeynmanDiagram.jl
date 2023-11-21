5e5917dee1a942ed20c052e36a6e09092f5d52ba
Lizhiyi <peter0627@mail.ustc.edu.cn>
2023-11-21 15:08:41 +0800

    add toMindspore.jl

--- src/backend/compiler.jl
+++ src/backend/compiler.jl
@@ -1,4 +1,5 @@
 module Compilers
+using PyCall
 using ..ComputationalGraphs
 import ..ComputationalGraphs: id, name, set_name!, operator, subgraphs, subgraph_factors, factor
 
@@ -8,5 +9,6 @@ using ..RuntimeGeneratedFunctions
 RuntimeGeneratedFunctions.init(Compilers)
 
 include("static.jl")
+include("toMindspore.jl")
 
 end
