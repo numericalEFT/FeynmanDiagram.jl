ad2b25d2720122793178f918c404cfd811b82e1b
dcerkoney <dcerkoney@gmail.com>
2023-12-20 17:00:22 -0500

    Bugfix in function `to_dot_str`

--- src/computational_graph/transform.jl
+++ src/computational_graph/transform.jl
@@ -183,7 +183,7 @@ end
     function flatten_chains(g::AbstractGraph) 
 
     Recursively flattens chains of subgraphs within a given graph `g` by merging certain trivial unary subgraphs into their parent graphs,
-    This function returns a new graph with flatten chains, dervied from the input graph `g` remaining unchanged.
+    This function returns a new graph with flatten chains, derived from the input graph `g` remaining unchanged.
 
 # Arguments:
 - `g::AbstractGraph`: graph to be modified
